03019dc301e8a95a2e072a3cec96b3b2e347360a
GameKing5678 <64851657+GameKing5678@users.noreply.github.com>
2023-05-18 10:13:52 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
 c = input('Enter operator ( either +, -, * or / )\n')
-
 a1 = input ('Enter first number: \n')
-
 b1 = input ('Enter second number: \n')
-
 a = float(a1)
-
 b = float(b1)
 
 if c == '+' :
-
   print("Your answer is",a+b) 
 
 if c == '-' :
-
   print ("Your answer is", a-b)
 
 if c == '*' :
-
   print ("Your answer is", a*b)
 
 if c == '/' :
-
   print ("Your answer is ", a/b)
-
+  
 print ("Thanks for using simple calculator \nMade by Gameking5678")
